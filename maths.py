e4f87ac4dca593d425899a929081656563b5697d
deva <deva@gmail.com>
2023-09-24 21:13:50 +0530

    Add/update feature maths

---
+++ maths.py
@@ -0,0 +1,11 @@
+a=26
+b=23
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+print("The addition of the value is:",add)
+print("The subtraction of the value is:",sub)
+print("The multiplication of the value is:",multi)
+print("The division of the value is:",div)
+print("This is sample one")
